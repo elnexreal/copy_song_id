dd56d2d4d43a1b492b6e06806d4a2e3a2fee1e5b
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-02-04 17:45:23 -0300

    changed the ui to look like the original (to not fuck jukebox)

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,31 @@
 using namespace geode::prelude;
 
 struct CopySongID : Modify<CopySongID, LevelInfoLayer> {
+    CustomSongWidget *songWidget;
+    CCLabelBMFont *songIDLabel;
+
     bool init(GJGameLevel *p0, bool p1) {
         if (!LevelInfoLayer::init(p0, p1))
             return false;
 
-        auto songWidget = this->m_songWidget;
+        songWidget = this->m_songWidget;
+        songIDLabel = songWidget->m_songIDLabel;
 
         if (!songWidget->m_isRobtopSong) {
-            // fix the more button away from the song creator or remove the button
-            for (int i = 0; i < songWidget->getChildren()->capacity(); i++) {
-                if (auto creatorLabel = typeinfo_cast<CCLabelBMFont *>(songWidget->getChildren()->objectAtIndex(i))) {
-                    if (std::string(creatorLabel->getString()).starts_with("By: ")) {
-                        creatorLabel->setPosition({-140.f, 16.f});
-                        creatorLabel->setScale(0.55f);
-                    }
-                }
-            }
+            songIDLabel->setVisible(false);
 
-            // hide the original label (not deleting it because game crash :sob:)
-            songWidget->m_songIDLabel->setVisible(false);
+            auto menuLayout = AxisLayout::create(Axis::Row);
+            menuLayout->setCrossAxisOverflow(false);
+            menuLayout->setAutoScale(false);
+            menuLayout->setAxisAlignment(AxisAlignment::Start);
+            menuLayout->setGap(30.0);
 
-            auto nodeLayout = AxisLayout::create(Axis::Column);
-            nodeLayout->setCrossAxisOverflow(false);
-            nodeLayout->setAutoScale(false);
-            nodeLayout->setCrossAxisAlignment(AxisAlignment::Start);
-            nodeLayout->setCrossAxisLineAlignment(AxisAlignment::Start);
-
-            auto node = CCMenu::create();
-            node->setPosition({-139.f, -22.f});
-            node->setAnchorPoint({0.f, 0.5f});
-            node->setContentSize({500.f, 75.f});
-            node->setLayout(nodeLayout);
-            node->setScale(0.35f);
+            auto menu = CCMenu::create();
+            menu->setPosition({-139.0, -32.0});
+            menu->setAnchorPoint({0.0, 0.5});
+            menu->setContentSize({600.0, 40.0});
+            menu->setLayout(menuLayout);
+            menu->setScale(0.35f);
 
             // strings for labels
             auto songIdStr = std::string("Song ID: ") + std::to_string(songWidget->m_customSongID);
@@ -55,22 +48,21 @@ struct CopySongID : Modify<CopySongID, LevelInfoLayer> {
             auto idBtn = CCMenuItemSpriteExtra::create(CCLabelBMFont::create(songIdStr.c_str(), "bigFont.fnt"),
                                                        this,
                                                        menu_selector(CopySongID::onButtonClick));
-            idBtn->setAnchorPoint({0.f, 0.5f});
+            idBtn->setAnchorPoint({0.0, 0.5});
             idBtn->setColor({192, 255, 199});
+            idBtn->m_startPosition = CCPoint(0.5, 0.5);
+            idBtn->m_scaleMultiplier = 1.05;
+            menu->addChild(idBtn);
 
             // create a sfx counter value
             if (songWidget->m_hasSFX) {
                 auto sfxLabel = CCLabelBMFont::create(sfxSizeStr.c_str(), "bigFont.fnt");
-                sfxLabel->setAnchorPoint({0.f, 0.5f});
+                sfxLabel->setAnchorPoint({0.0, 0.5});
 
-                node->addChild(sfxLabel);
+                menu->addChild(sfxLabel);
             }
-
-            node->addChild(idBtn);
-
-            node->updateLayout();
-
-            songWidget->addChild(node);
+            menu->updateLayout();
+            songWidget->addChild(menu);
         }
 
         return true;
@@ -80,8 +72,9 @@ struct CopySongID : Modify<CopySongID, LevelInfoLayer> {
         auto songWidget = this->m_songWidget;
 
         try {
-            Notification::create(std::string("ID ") + std::to_string(songWidget->m_customSongID) + std::string(" successfully copied"), CCSprite::createWithSpriteFrameName("GJ_completesIcon_001.png"))->show();
+            // copy the song ID to the clipboard
             clipboard::write(std::to_string(songWidget->m_customSongID));
+            Notification::create(std::string("ID ") + std::to_string(songWidget->m_customSongID) + std::string(" successfully copied"), CCSprite::createWithSpriteFrameName("GJ_completesIcon_001.png"))->show();
         } catch (const std::exception &e) {
             Notification::create(std::string("Failed to copy ID"), CCSprite::createWithSpriteFrameName("GJ_deleteIcon_001.png"))->show();
         }
